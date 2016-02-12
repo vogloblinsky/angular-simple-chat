94dd3d6da1bf5a658ea65f4b31b8fbff2b6c1d76
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-02-12 22:13:49 +0100

    Remove IIFE

--- src/js/directives/simple-chat/simple-chat.config.service.js
+++ src/js/directives/simple-chat/simple-chat.config.service.js
@@ -1,24 +1,20 @@
-(function() {
-    'use strict';
+angular
+    .module('angular-simple-chat.directives')
+    .service('SimpleChatConfiguration', SimpleChatConfiguration);
 
-    angular
-        .module('angular-simple-chat.directives')
-        .service('SimpleChatConfiguration', SimpleChatConfiguration);
+SimpleChatConfiguration.$inject = [];
 
-    SimpleChatConfiguration.$inject = [];
+/* @ngInject */
+function SimpleChatConfiguration() {
+    var _options = {
+        showUserAvatar: true
+    };
 
-    /* @ngInject */
-    function SimpleChatConfiguration() {
-        var _options = {
-            showUserAvatar: true
-        };
+    this.options = function() {
+        return _options;
+    };
 
-        this.options = function() {
-            return _options;
-        };
-
-        this.setShowUserAvatar = function(value) {
-            _options.showUserAvatar = value;
-        };
-    }
-})();
+    this.setShowUserAvatar = function(value) {
+        _options.showUserAvatar = value;
+    };
+}
