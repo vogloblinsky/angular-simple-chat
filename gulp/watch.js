ca5945443f000febacc5e2510b0e89a800bd1a71
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2016-02-23 22:18:56 +0100

    Fix examples folder for watch

--- gulp/watch.js
+++ gulp/watch.js
@@ -3,7 +3,7 @@
 var gulp = require('gulp');
 
 gulp.task('watch', ['styles-dev', 'scripts-dev', 'templates'], function() {
-    gulp.watch(['./example/**/*.html'], ['serve-reload']);
+    gulp.watch(['./examples/**/*.html'], ['serve-reload']);
     gulp.watch(['./src/**/*.js', './example/**/*.js'], ['serve-reload']);
     gulp.watch(['./src/**/*.html'], ['templates-reload']);
     gulp.watch(['./src/**/*.scss'], ['sass-reload']);
